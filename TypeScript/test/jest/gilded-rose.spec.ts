74dac91d1f3899e0941f526c51ca9cb0c7eb7663
Luis Bosch <luis.bosch@gmail.com>
2023-11-10 17:37:47 +1300

    Added test cases for the regular items
    - Tried to understand the system better
    - Start to get some unit tests in
    - The idea es to get enough coverage before we update for new requirements

--- TypeScript/test/jest/gilded-rose.spec.ts
+++ TypeScript/test/jest/gilded-rose.spec.ts
@@ -1,9 +1,35 @@
 import { Item, GildedRose } from '@/gilded-rose';
 
 describe('Gilded Rose', () => {
-  it('should foo', () => {
-    const gildedRose = new GildedRose([new Item('foo', 0, 0)]);
-    const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe('fixme');
+  describe('At the end of each day our system lowers both values for every item', () => {
+    describe('Regular item', () => {
+      it('should lower sellIn by 1 and quality by 1', () => {
+        const gildedRose = new GildedRose([new Item('regular', 10, 8)]);
+        const items = gildedRose.updateQuality();
+        expect(items[0].name).toBe('regular');
+        expect(items[0].sellIn).toBe(9);
+        expect(items[0].quality).toBe(7);
+      });
+      it('should quality by 2 if sell date has passed', () => {
+        const gildedRose = new GildedRose([new Item('regular', 0, 8)]);
+        const items = gildedRose.updateQuality();
+        expect(items[0].name).toBe('regular');
+        expect(items[0].sellIn).toBe(-1);
+        expect(items[0].quality).toBe(6);
+      });
+      it('quality shouldnt be negative', () => {
+        const gildedRose = new GildedRose([new Item('regular', 10, 0), new Item('regular', 0, 0)]);
+        const items = gildedRose.updateQuality();
+        expect(items[0].name).toBe('regular');
+        expect(items[0].sellIn).toBe(9);
+        expect(items[0].quality).toBe(0);
+
+        // edge case
+        expect(items[1].name).toBe('regular');
+        expect(items[1].sellIn).toBe(-1);
+        expect(items[1].quality).toBe(0);
+      });
+    });
   });
+
 });
